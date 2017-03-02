79ffa4c1edd03aa956410efe4bba0f94bce74835
HAILSATAN420 <SKNK929@GMAIL.COM>
2017-03-01 22:10:35 -0500

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,56 @@
+function Script() {
+	this.chatHistory = ["¯\\_(ツ)_/¯", "(╯°□°）╯︵ ┻━┻", "\┬\─\┬ ノ( ゜-゜ノ)", "ಠ_ಠ",  "(=ↀωↀ=)", "(ↀДↀ)⁼³₌₃", "(=ｘェｘ=)", "(≚ᄌ≚)ƶƵ","ヽ(=^･ω･^=)丿",
+	"(ↀДↀ)⁼³₌₃", "ლ(=ↀωↀ=)ლ", "(U・x・U)", "＜コ：ミ","ヽ༼ ಠ益ಠ ༽ﾉ", "凸(ಠ益ಠ)凸",
+ ];
+
+	this.currentItem = 0;
+
+	this.startAutoWoot();
+	this.setupChatHistory();
+
+	$("#header-global").append("<font id='DUBTRACK-TEXT' size='2'><font color='red'>ENABLED</font></font>");
+}
+
+Script.prototype.setupChatHistory = function() {
+	var _this = this;
+	var updateChatFromHistory = function() {
+		$('#chat-txt-message').val( _this.chatHistory[_this.currentItem]);
+	};
+	$("#chat-txt-message").keydown(function(event) {
+		if(event.keyCode != 38 && event.keyCode != 40)
+			return;
+
+		if( event.keyCode == 38 && _this.currentItem > 0) {
+			_this.currentItem -= 1;
+		}
+
+		if( event.keyCode == 40 && _this.currentItem < _this.chatHistory.length ) {
+			_this.currentItem += 1;
+		}
+		updateChatFromHistory();
+	});
+}
+Script.prototype.startAutoWoot = function() {
+	 $(".dubup").click();
+
+	this.autoWootTimer = setInterval( function() {
+		 $(".dubup").click();
+	}, 45000);
+}
+
+Script.prototype.stopAutoWoot = function() {
+	if( this.autoWootTimer != undefined )
+		clearInterval( this.autoWootTimer );
+}
+
+Script.prototype.cleanUp = function() {
+	this.stopAutoWoot();
+	this.isDisabled = true;
+	$("DUBTRACK-TEXT").remove();
+}
+if( document.Script === undefined ) {
+	document.Script = new Script();
+} else {
+	document.Script.cleanUp();
+	document.Script = undefined;
+}
